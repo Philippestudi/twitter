fb96317c7005243707c0b29d8ae8440957bd2c9c
balestri <philippebalestri67@gmail.com>
2025-03-24 10:26:31 +0100

    ajout certificat prod

--- environment/production.js
+++ environment/production.js
@@ -1,7 +1,5 @@
-const path = require('path');
-
 module.exports = {
   dbUrl: 'mongodb+srv://philippebalestri67:ygpq9OnSJHfGKzKU@cluster0.pox3g.mongodb.net/twitter?retryWrites=true&w=majority&appName=Cluster0',
-  cert: path.join( __dirname, '../ssl/localhost.crt'),
-  key: path.join( __dirname, '../ssl/localhost.key'),
+  cert: '/etc/letsencrypt/live/www.twittare.site/fullchain.pem',
+  key: '/etc/letsencrypt/live/www.twittare.site/privkey.pem',
 }
